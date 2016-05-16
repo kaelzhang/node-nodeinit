c7fe79567bbe5c8913b4d863ad66a1e58826bf8b
Kael <i@kael.me>
2016-05-16 14:08:57 +0800

    1.3.3: installation

--- root/install.sh
+++ root/install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-npm install
-
-# babel fails old node, so install babel manually
-type babel || npm i -g babel babel-preset-es2015
+# babel-preset-es2015 fails to install on nodejs < 0.10
+npm install babel-cli babel-preset-es2015
