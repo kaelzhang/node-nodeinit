1131abe4b72280d593d87e4fceaec62226a2f5d3
Kael <i@kael.me>
2016-07-12 17:42:07 +0800

    2.0.0: bash test.sh

--- root/install.sh
+++ root/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # babel-preset-es2015 fails to install on nodejs < 0.10
+npm install
 npm install babel-cli babel-preset-es2015
--- root/test.sh
+++ root/test.sh
@@ -3,3 +3,5 @@
 export PATH=$PATH:./node_modules/.bin/
 
 type babel && babel src --out-dir lib --presets es2015 || echo 'warn: babel failed'
+
+mocha --reporter spec ./test/*.js
