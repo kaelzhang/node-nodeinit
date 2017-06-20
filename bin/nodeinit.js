b82d8aeb573274d53fc9ac296b2368c7436b4cf3
Kael Zhang <i+github@kael.me>
2017-06-20 15:17:20 +0800

    bump version 3.6.6: upgrade scaffold-generator

--- bin/nodeinit.js
+++ bin/nodeinit.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-var gu = require('githuburl');
-var scaffold = require('scaffold-generator');
-var node_path = require('path');
-var mkdirp = require('mkdirp');
-var fs = require('fs');
-var ejs = require('ejs');
-ejs.open = '{{';
-ejs.close = '}}';
+var gu = require('githuburl')
+var Scaffold = require('scaffold-generator')
+var node_path = require('path')
+var mkdirp = require('mkdirp')
+var fs = require('fs')
+var mustache = require('mustache')
 
-var spawns = require('spawns');
-var ini = require('ini');
 
-var argv = require('minimist')(process.argv.slice(2));
+var spawns = require('spawns')
+var ini = require('ini')
 
-var repo = argv._.pop();
+var argv = require('minimist')(process.argv.slice(2))
+
+var repo = argv._.pop()
 
 function fail (message, code) {
-  process.stderr.write(message + '\n');
-  process.exit(code || 1);
+  process.stderr.write(message + '\n')
+  process.exit(code || 1)
 }
 
 function log (message) {
-  process.stdout.write(message + '\n');
+  process.stdout.write(message + '\n')
 }
 
 
@@ -35,38 +34,38 @@ var Usage = [
   '  --npm_user <npm-user>',
   '  --user <user>'
 
-].join('\n');
+].join('\n')
 
 
 if (!repo) {
-  fail(Usage);
+  fail(Usage)
 }
 
-var parsed = gu(repo);
-var basename = parsed.repo;
-var name = parsed.repo.replace(/^node-/i, '');
-var js_name = name.replace(/^\d+/, '').replace(/-/g, '_');
+var parsed = gu(repo)
+var basename = parsed.repo
+var name = parsed.repo.replace(/^node-/i, '')
+var js_name = name.replace(/^\d+/, '').replace(/-/g, '_')
 
-var home = require('home-dir')();
-var config_file = node_path.join(home, '.nodeinit');
+var home = require('home-dir')()
+var config_file = node_path.join(home, '.nodeinit')
 
-var config_exists = fs.existsSync(config_file);
+var config_exists = fs.existsSync(config_file)
 var config = config_exists
   ? ini.parse( fs.readFileSync(config_file).toString() )
-  : {};
+  : {}
 
-var email = argv.email || config.email;
+var email = argv.email || config.email
 
 if (!email) {
   fail(
     'email is not configured, `--email` option should be specified:\n' +
     '\n' +
     '   nodeinit ' + repo + ' --email <email>'
-  );
+  )
 }
 
 if (!config.email && argv.email) {
-  fs.writeFileSync(config_file, ini.stringify(argv));
+  fs.writeFileSync(config_file, ini.stringify(argv))
 }
 
 var data = {
@@ -78,32 +77,29 @@ var data = {
   npm_user: argv.npm_user || config.npm_user || parsed.user,
   ignore_file: '.gitignore',
   npm_ignore_file: '.npmignore'
-};
+}
 
-var cwd = argv.cwd || process.cwd();
-var dir = node_path.join(cwd, name);
+var cwd = argv.cwd || process.cwd()
+var dir = node_path.join(cwd, name)
 
 // Yeah, I used a lot of sync fs here which is not a good practice.
 // But nodeinit is not a project that concerns high performance.
 // So, just make it simple
 if ( fs.existsSync(dir) ) {
-  fail('Directory "' + name + '" already exists');
+  fail('Directory "' + name + '" already exists')
 } else {
-  mkdirp.sync(dir);
+  mkdirp.sync(dir)
 }
 
-var template_root = node_path.join(__dirname, '..', 'root');
+var template_root = node_path.join(__dirname, '..', 'root')
 
-scaffold({
+new Scaffold({
   data: data,
   override: argv.force,
-  renderer: ejs
+  render: mustache.render
 
-}).copy(template_root, dir, function (err) {
-  if (err) {
-    fail(err.stack);
-  }
-
+}).copy(template_root, dir)
+.then(() => {
   spawns([
     'git init',
     'git add -A',
@@ -116,6 +112,9 @@ scaffold({
     cwd: dir
 
   }, function (code, signal) {
-    process.exit(code);
-  });
-});
+    process.exit(code)
+  })
+})
+.catch(err => {
+  fail(err.stack)
+})
