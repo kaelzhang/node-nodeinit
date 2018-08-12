7f4c65ca36a01e906f96f4354c25e5ff2d5bc1e1
Kael Zhang <i+github@kael.me>
2018-08-12 08:50:30 +0800

    4.1.11: require sequence

--- root/test/{{name}}.js
+++ root/test/{{name}}.js
@@ -1,6 +1,6 @@
 const {test} = require('ava')
-const {{js_name}} = require('../src')
 const log = require('util').debuglog('{{name}}')
+const {{js_name}} = require('../src')
 
 test('description', t => {
   t.is(true, true)
